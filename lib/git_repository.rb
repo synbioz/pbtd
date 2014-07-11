89799e79113cc13ed5dc45e7f7d111d8175627a6
Théo Delaune <tdelaune@synbioz.com>
2014-07-11 14:57:50 +0200

    add doc for module Pbtd

--- lib/git_repository.rb
+++ lib/git_repository.rb
@@ -1,3 +1,7 @@
+
+#
+# Push Button To Deploy manage git repository
+#
 module Pbtd
 
     #
@@ -5,9 +9,19 @@ module Pbtd
     # git repository through 'rugged' gem
     #
     class GitRepository
+      # @return [String]
       attr_reader :repository_url, :username
+      # @return [Rugged::Repository]
       attr_accessor :rugged_repository
 
+      #
+      # Initializer for Pbtd::GitRepository class with optional git repository url
+      # only SSH connection was implemented.
+      # Syntax supported like : "user@domain:repository.git"
+      #
+      # @param repo_url [String] [git repository url, if blank it initialize empty Pbtd::GitRepository]
+      #
+      # @return [Pbtd::GitRepository]
       def initialize(repo_url=nil)
         unless repo_url.blank?
           @repository_url = repo_url
@@ -15,10 +29,21 @@ module Pbtd
         end
       end
 
+      #
+      # Open existing and already cloned git repository
+      # @param repo_name [String] [repository folder name]
+      #
+      # @return [Rugged::Repository]
       def open(repo_name)
         @rugged_repository = Rugged::Repository.new(in_path(repo_name))
       end
 
+      #
+      # Clone a previous initialized Pbtd::GitRepository if
+      # it has repository_url and username setted.
+      # @param name_repository [String] [folder name for cloned repository]
+      #
+      # @return [Rugged::Repository]
       def clone(name_repository)
         begin
           @rugged_repository = Rugged::Repository.clone_at(repository_url, in_path(name_repository), credentials: credentials)
@@ -29,14 +54,27 @@ module Pbtd
         end
       end
 
+      #
+      # Return remote branches of the repository
+      #
+      # @return [Array] [array of branches name]
       def remote_branches
         rugged_repository.branches.each_name(:remote).sort
       end
 
+      #
+      # get the last commit of branch
+      # @param branch_name [String] [branch name]
+      #
+      # @return [Rugged::Commit]
       def last_commit(branch_name)
         rugged_repository.branches[branch_name].target
       end
 
+      #
+      # `git fetch` repository
+      #
+      # @return [Integer] [number of git total deltas]
       def fetch
         remote = rugged_repository.remotes['origin']
 
@@ -46,6 +84,11 @@ module Pbtd
         remote.fetch(credentials: credentials)[:total_deltas]
       end
 
+      #
+      # get number of commits between local branch and remote branch
+      # @param branch_name [String] [local branch name]
+      #
+      # @return [Integer] [number of commits]
       def get_ahead(branch_name)
         local_commit = last_commit(branch_name)
         remote_commit = last_commit(remote_branch_from_local(branch_name))
@@ -53,6 +96,11 @@ module Pbtd
         rugged_repository.ahead_behind(local_commit, remote_commit).first
       end
 
+      #
+      # Merge the previously fetched remote branch to your local branch
+      # @param branch_name [String] [local branch name]
+      #
+      # @return [Rugged::Reference]
       def merge(branch_name)
         remote_commit = last_commit(remote_branch_from_local(branch_name))
 
@@ -60,22 +108,39 @@ module Pbtd
       end
 
       private
+        #
+        # set path where repository is stored
+        # @param name_repository [String] [folder repository name]
+        #
+        # @return [String]
         def in_path(name_repository)
           SETTINGS["repositories_path"] + '/' + name_repository
         end
 
+        #
+        # use Rugged::Credentials::SshKeyFromAgent for create ssh credentials for user
+        #
+        # @return [Rugged::Credentials::SshKeyFromAgent]
         def credentials
           Rugged::Credentials::SshKeyFromAgent.new(username: username)
         end
 
+        #
+        # find remote branch name from local branch name
+        # @param branch_name [String] [local branch name]
+        #
+        # @return [String]
         def remote_branch_from_local(branch_name)
           remote_branches.find { |x| x.ends_with?(branch_name) }
         end
     end
 
+    #
+    # Custom Errors
+    #
     module Error
       exceptions = %w[ GitRepositoryNotFound Network ]
-
+      # Create empty empty class from exceptions array
       exceptions.each { |e| const_set(e, Class.new(StandardError)) }
     end
 end
