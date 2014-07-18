eba4e3f80f0006abd32b913fb5654ed08f6ad8c4
Théo Delaune <tdelaune@synbioz.com>
2014-07-18 17:23:37 +0200

    store sidekiq job in Worker model

---
+++ app/workers/git_clone_worker.rb
@@ -0,0 +1,27 @@
+class GitCloneWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :retry => false
+
+  def perform(project_id)
+    project = Project.find(project_id)
+    project.worker = Worker.create(job_id: self.jid, class_name: self.class.name)
+    project.worker.running!
+    project.save
+
+    begin
+      clone(project)
+      project.worker.success!
+    rescue => e
+      project.worker.error_class_name = e.class.name
+      project.worker.error_message = e.message
+      project.worker.failure!
+      raise e
+    end
+  end
+
+  def clone(project)
+    repo = Pbtd::GitRepository.new(project.repository_url)
+    repo.clone(project.name)
+  end
+end
