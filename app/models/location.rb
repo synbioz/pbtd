1fbf493d75e59618c7baab5ee91fe5d171e19bf3
Théo Delaune <tdelaune@synbioz.com>
2014-07-25 10:27:51 +0200

    WIP distance sidekiq worker

--- app/models/location.rb
+++ app/models/location.rb
@@ -25,16 +25,20 @@ class Location < ActiveRecord::Base
   #
   # @return [Integer]
   def get_distance_from_release
-    cap_lib_path = Rails.root.join('lib', 'pbtd', 'capistrano')
-    project_path = File.join(SETTINGS["repositories_path"], self.project.name)
 
-    `cd #{project_path} && bundle install`
-    release_commit_sha = `cap #{self.name} -R #{cap_lib_path} remote:fetch_revision`.strip
 
     repo = Pbtd::GitRepository.new
     repo.open(self.project.name)
     repo.fetch
     repo.checkout(self.branch)
     repo.get_behind(self.branch, release_commit_sha)
+  end
+
+  def get_current_release_commit
+    cap_lib_path = Rails.root.join('lib', 'pbtd', 'capistrano')
+    project_path = File.join(SETTINGS["repositories_path"], self.project.name)
+
+    `cd #{project_path} && bundle install`
+    release_commit_sha = `cap #{self.name} -R #{cap_lib_path} remote:fetch_revision`.strip
   end
 end
---
+++ app/workers/distance_worker.rb
@@ -0,0 +1,18 @@
+class DistanceWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :retry => false
+
+  def perform(location_id)
+    location = Location.find(location_id)
+
+    repo = Pbtd::GitRepository.new
+    repo.open(location.project.name)
+    repo.fetch
+    repo.checkout(location.branch)
+
+    distance = repo.get_behind(location.branch, location.get_current_release_commit)
+
+    location.update_attribute(:distance, distance)
+  end
+end
