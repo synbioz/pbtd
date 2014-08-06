39715464f4fb71924df7409088b5025ec04e76e0
Théo Delaune <tdelaune@synbioz.com>
2014-08-06 15:58:08 +0200

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with realm: 'Pbtd access', name:'synbioz', password: 'g1tPbtdT0ol', if: lambda { !Rails.env.development? }
 end
