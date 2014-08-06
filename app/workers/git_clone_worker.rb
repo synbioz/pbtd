b73cc92ad942dc83b28046ef18745a28feb16e77
Théo Delaune <tdelaune@synbioz.com>
2014-08-06 15:49:46 +0200

    remove useless puts

--- app/workers/git_clone_worker.rb
+++ app/workers/git_clone_worker.rb
@@ -6,9 +6,6 @@ class GitCloneWorker
   def perform(project_id, default_branch=nil)
     project = Project.find(project_id)
     project.worker.destroy if project.worker
-    puts "errors"
-    puts project.inspect
-    puts default_branch.inspect
     project.worker = Worker.create(job_id: self.jid, class_name: self.class.name)
     project.worker.running!
     project.save
