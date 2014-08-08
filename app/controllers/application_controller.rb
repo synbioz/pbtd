f6fd433728a5633a84443694f742ea21a6fe7979
Théo Delaune <tdelaune@synbioz.com>
2014-08-08 09:55:21 +0200

    add without basic auth for dev && test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with realm: 'Pbtd access', name:'synbioz', password: 'g1tPbtdT0ol', if: lambda { !Rails.env.test? }
+  http_basic_authenticate_with realm: 'Pbtd access', name:'synbioz', password: 'g1tPbtdT0ol', if: lambda { !(Rails.env.test? || Rails.env.development?) }
 end
