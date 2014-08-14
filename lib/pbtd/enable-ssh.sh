a4b2d0a895ef3dd93cd1b145a3ad6e3699d80a10
Théo Delaune <tdelaune@synbioz.com>
2014-08-14 09:22:07 +0200

    clear bash file for ssh-agent

--- lib/pbtd/enable-ssh.sh
+++ lib/pbtd/enable-ssh.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-export SSH_AUTH_SOCK=/tmp/ssh-agent-socket
+agent-socket=/var/www/pbtd/shared/tmp/sockets/ssh-agent-socket
+agent-export=/var/www/pbtd/shared/tmp/ssh/ssh-export-agent
+agent-pid=/var/www/pbtd/shared/tmp/pids/ssh-agent.pid
+ssh-key=/var/www/pbtd/shared/tmp/ssh/pbtd_key
 
 start_agent() {
-  ssh-agent -s -a $SSH_AUTH_SOCK > /tmp/ssh-export-agent
-  . /tmp/ssh-export-agent
-  echo $SSH_AGENT_PID > /tmp/ssh-agent.pid
+  ssh-agent -s -a $agent-socket > $agent-export
+  . $agent-export
+  echo $SSH_AGENT_PID > $agent-pid
 }
 
 if [ -z "$SSH_AGENT_PID" ] ; then start_agent; fi
@@ -15,5 +18,5 @@ fi
 
 ssh-add -l > /dev/null 2>&1
 if [ $? -ne 0 ]; then
- ssh-add ~/ssh/my_key > /dev/null 2>&1
+ ssh-add $ssh-key > /dev/null 2>&1
 fi
