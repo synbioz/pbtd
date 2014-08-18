fab26509a86bd8bb4e59a8be7ccd2098d548d99b
Théo Delaune <tdelaune@synbioz.com>
2014-08-18 09:11:04 +0200

    add errbit tracking for sidekiq workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://127.0.0.1:6379/12', namespace: 'pbtd' }
+  config.error_handlers << Proc.new { |ex,ctx_hash| Airbrake.notify_or_ignore(ex, ctx_hash) }
 end
 
 Sidekiq.configure_client do |config|
