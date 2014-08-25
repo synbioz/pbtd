cde4f72740481a9c31c9b20e5b771108fa086b2f
Théo Delaune <tdelaune@synbioz.com>
2014-08-25 16:01:27 +0200

    add comment for bundle install and headers problem

--- app/models/location.rb
+++ app/models/location.rb
@@ -45,6 +45,8 @@ class Location < ActiveRecord::Base
 
     sha = nil
 
+    # Bundle install cannot install headers for specific gems (ex: 'sqlite')
+    # Please install it on remote servers
     if cap_version < "3.0.0"
       sha = Bundler.with_clean_env do
         `cd #{project_path} 2> /dev/null && bundle install --deployment 2> /dev/null && #{SETTINGS["ssh_agent_script"]} bundle exec cap #{self.name} -Ff #{cap_2_lib_path}/revision.rake remote:fetch_revision`
