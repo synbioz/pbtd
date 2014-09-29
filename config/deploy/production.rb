1cdc6e63f5f2b82445f7c24914c066511c6b36af
Théo Delaune <tdelaune@synbioz.com>
2014-09-29 17:33:29 +0200

    remove real environments for capistrano

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-server 'pbtd.synbioz.com', user: 'synbioz', roles: %w{web app db}
-
-set :rails_env, "production"
-
-set :branch, "master"
--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-server 'pbtd.dev.synbioz.com', user: 'synbioz', roles: %w{web app db}
-
-set :rails_env, 'staging'
-
-set :branch, 'develop'
-
-after 'deploy', 'copy_no_robots_file'
